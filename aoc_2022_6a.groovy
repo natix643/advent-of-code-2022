467bfc55ee5f32a95955b78b6cd552e65ce84c4a
Jiří Machart <natix643@gmail.com>
2022-12-07 10:45:00 +0100

    fixed parts order, cleanup

--- aoc_2022_6a.groovy
+++ aoc_2022_6a.groovy
@@ -2,7 +2,8 @@ testInput = 'mjqjpqmgbljsphdztnvjfqwrcgsmlb'
 realInput = new File(/C:\Users\jirka\IdeaProjects\advent-of-code-2022\aoc_2022_06.txt/).readLines()[0]
 input = realInput.toList()
 
-def entry = input.collate(14, 1, false).indexed().find { index, quartet ->
-    quartet.toSet().size() == 14
-}
-entry.key + 14
+windowSize = 4
+
+input.collate(windowSize, 1, false).findIndexOf {
+    it.toSet().size() == windowSize
+} + windowSize
