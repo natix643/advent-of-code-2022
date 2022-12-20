1478809331b0def9882d307b7139bcaf510045ee
Jiří Machart <natix643@gmail.com>
2022-12-20 23:53:50 +0100

    speed up

--- aoc_2022_20a.groovy
+++ aoc_2022_20a.groovy
@@ -12,47 +12,56 @@ testInput = '''
 realInput = new File(/C:\Users\jirka\IdeaProjects\advent-of-code-2022\aoc_2022_20.txt/)
 lines = realInput.readLines().findAll()
 
-@TupleConstructor
 class Reference {
-    final int number
-
-    String toString() {
-        this.number
-    }
+    int number
 }
 
 void println(List<Reference> references) {
-    println(references.join(', '))
+    println(references*.number.join(', '))
 }
 
-void shiftRight(List<?> list, int index) {
+int shiftRight(List<?> list, int index) {
     if (index == list.size() - 1) {
         Collections.rotate(list, 1)
         list.swap(0, 1)
+        return 1
     } else {
         def to = index + 1
         list.swap(index, to)
         if (to == list.size() - 1) {
             Collections.rotate(list, 1)
+            return 0
+        } else {
+            return to
         }
     }
 }
 
-void shiftLeft(List<?> list, int index) {
+int shiftLeft(List<?> list, int index) {
     if (index == 0) {
         Collections.rotate(list, -1)
         list.swap(list.size() - 2, list.size() - 1)
+        return list.size() - 2
     } else {
         def to = index - 1
         list.swap(index, to)
         if (to == 0) {
             Collections.rotate(list, -1)
+            return list.size() - 1
+        } else {
+            return to
         }
     }
 }
 
+int findAtIndex(List<Reference> references, int index) {
+    def zeroIndex = references.findIndexOf { it.number == 0 }
+    def normalizedIndex = (zeroIndex + index) % references.size()
+    return references[normalizedIndex].number
+}
+
 def sourceNumbers = lines.collect {
-    new Reference(it.toInteger())
+    new Reference(number: it.toInteger())
 }
 def targetNumbers = sourceNumbers.toList()
 
@@ -64,24 +73,18 @@ sourceNumbers.each { reference ->
 //    println "\n$number moves"
 
     if (number > 0) {
+        def index = targetNumbers.indexOf(reference)
         number.times {
-            def index = targetNumbers.indexOf(reference)
-            shiftRight(targetNumbers, index)
+            index = shiftRight(targetNumbers, index)
 //            println targetNumbers
         }
     } else {
+        def index = targetNumbers.indexOf(reference)
         number.abs().times {
-            def index = targetNumbers.indexOf(reference)
-            shiftLeft(targetNumbers, index)
+            index = shiftLeft(targetNumbers, index)
 //            println targetNumbers
         }
     }
-}
-
-int findAtIndex(List<Reference> references, int index) {
-    def zeroIndex = references.findIndexOf { it.number == 0 }
-    def normalizedIndex = (zeroIndex + index) % references.size()
-    return references[normalizedIndex].number
 }
 
 def result = findAtIndex(targetNumbers, 1000) + findAtIndex(targetNumbers, 2000) + findAtIndex(targetNumbers, 3000)
